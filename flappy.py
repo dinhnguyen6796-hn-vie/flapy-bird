aef21b9c1cb07e3d7b5e6ce877dafad4a0048e07
Dinh Nguyen <dinhnguyen6796@gmail.com>
2026-05-17 15:10:47 +0700

    update gameplay

--- flappy.py
+++ flappy.py
@@ -18,6 +18,7 @@ PIPE_GAP = 150
 
 wing = 'assets/audio/wing.wav'
 hit = 'assets/audio/hit.wav'
+point = 'assets/audio/point.wav'
 
 pygame.mixer.init()
 
@@ -57,16 +58,13 @@ class Bird(pygame.sprite.Sprite):
         self.image = self.images[self.current_image]
 
 
-
-
 class Pipe(pygame.sprite.Sprite):
 
     def __init__(self, inverted, xpos, ysize):
         pygame.sprite.Sprite.__init__(self)
 
-        self. image = pygame.image.load('assets/sprites/pipe-green.png').convert_alpha()
+        self.image = pygame.image.load('assets/sprites/pipe-green.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (PIPE_WIDHT, PIPE_HEIGHT))
-
 
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
@@ -77,14 +75,12 @@ class Pipe(pygame.sprite.Sprite):
         else:
             self.rect[1] = SCREEN_HEIGHT - ysize
 
-
         self.mask = pygame.mask.from_surface(self.image)
-
+        self.passed = False  # dùng để đếm điểm
 
     def update(self):
         self.rect[0] -= GAME_SPEED
 
-        
 
 class Ground(pygame.sprite.Sprite):
     
@@ -98,11 +94,14 @@ class Ground(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
         self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
+
     def update(self):
         self.rect[0] -= GAME_SPEED
 
+
 def is_off_screen(sprite):
     return sprite.rect[0] < -(sprite.rect[2])
+
 
 def get_random_pipes(xpos):
     size = random.randint(100, 300)
@@ -111,112 +110,187 @@ def get_random_pipes(xpos):
     return pipe, pipe_inverted
 
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
-pygame.display.set_caption('Flappy Bird')
+def draw_score(screen, score):
+    """Vẽ điểm số lên màn hình bằng sprite số."""
+    score_str = str(score)
+    total_width = 0
+    digit_images = []
+    for ch in score_str:
+        img = pygame.image.load(f'assets/sprites/{ch}.png').convert_alpha()
+        digit_images.append(img)
+        total_width += img.get_width()
 
-BACKGROUND = pygame.image.load('assets/sprites/background-day.png')
-BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDHT, SCREEN_HEIGHT))
-BEGIN_IMAGE = pygame.image.load('assets/sprites/message.png').convert_alpha()
-
-bird_group = pygame.sprite.Group()
-bird = Bird()
-bird_group.add(bird)
-
-ground_group = pygame.sprite.Group()
-
-for i in range (2):
-    ground = Ground(GROUND_WIDHT * i)
-    ground_group.add(ground)
-
-pipe_group = pygame.sprite.Group()
-for i in range (2):
-    pipes = get_random_pipes(SCREEN_WIDHT * i + 800)
-    pipe_group.add(pipes[0])
-    pipe_group.add(pipes[1])
+    x = (SCREEN_WIDHT - total_width) // 2
+    for img in digit_images:
+        screen.blit(img, (x, 20))
+        x += img.get_width()
 
 
+def show_gameover_screen(screen, score):
+    """Hiển thị màn hình game over và chờ người chơi nhấn phím để chơi lại hoặc thoát."""
+    GAMEOVER_IMAGE = pygame.image.load('assets/sprites/gameover.png').convert_alpha()
+    BACKGROUND = pygame.image.load('assets/sprites/background-day.png')
+    BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDHT, SCREEN_HEIGHT))
 
-clock = pygame.time.Clock()
+    font_big = pygame.font.SysFont('Arial', 36, bold=True)
+    font_small = pygame.font.SysFont('Arial', 22)
 
-begin = True
+    while True:
+        screen.blit(BACKGROUND, (0, 0))
 
-while begin:
+        # Gameover image
+        go_rect = GAMEOVER_IMAGE.get_rect(center=(SCREEN_WIDHT // 2, SCREEN_HEIGHT // 2 - 60))
+        screen.blit(GAMEOVER_IMAGE, go_rect)
 
-    clock.tick(15)
+        # Hiển thị điểm
+        score_text = font_big.render(f'Score: {score}', True, (255, 255, 255))
+        score_rect = score_text.get_rect(center=(SCREEN_WIDHT // 2, SCREEN_HEIGHT // 2 + 20))
+        screen.blit(score_text, score_rect)
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE or event.key == K_UP:
-                bird.bump()
-                pygame.mixer.music.load(wing)
-                pygame.mixer.music.play()
-                begin = False
+        # Hướng dẫn
+        hint_text = font_small.render('SPACE / UP: Choi lai   ESC: Thoat', True, (255, 255, 255))
+        hint_rect = hint_text.get_rect(center=(SCREEN_WIDHT // 2, SCREEN_HEIGHT // 2 + 70))
+        screen.blit(hint_text, hint_rect)
 
-    screen.blit(BACKGROUND, (0, 0))
-    screen.blit(BEGIN_IMAGE, (120, 150))
+        pygame.display.update()
 
-    if is_off_screen(ground_group.sprites()[0]):
-        ground_group.remove(ground_group.sprites()[0])
-
-        new_ground = Ground(GROUND_WIDHT - 20)
-        ground_group.add(new_ground)
-
-    bird.begin()
-    ground_group.update()
-
-    bird_group.draw(screen)
-    ground_group.draw(screen)
-
-    pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    pygame.quit()
+                    exit()
+                if event.key == K_SPACE or event.key == K_UP:
+                    return  # chơi lại
 
 
-while True:
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
+    pygame.display.set_caption('Flappy Bird')
 
-    clock.tick(15)
+    BACKGROUND = pygame.image.load('assets/sprites/background-day.png')
+    BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDHT, SCREEN_HEIGHT))
+    BEGIN_IMAGE = pygame.image.load('assets/sprites/message.png').convert_alpha()
 
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-        if event.type == KEYDOWN:
-            if event.key == K_SPACE or event.key == K_UP:
-                bird.bump()
-                pygame.mixer.music.load(wing)
-                pygame.mixer.music.play()
+    bird_group = pygame.sprite.Group()
+    bird = Bird()
+    bird_group.add(bird)
 
-    screen.blit(BACKGROUND, (0, 0))
+    ground_group = pygame.sprite.Group()
+    for i in range(2):
+        ground = Ground(GROUND_WIDHT * i)
+        ground_group.add(ground)
 
-    if is_off_screen(ground_group.sprites()[0]):
-        ground_group.remove(ground_group.sprites()[0])
-
-        new_ground = Ground(GROUND_WIDHT - 20)
-        ground_group.add(new_ground)
-
-    if is_off_screen(pipe_group.sprites()[0]):
-        pipe_group.remove(pipe_group.sprites()[0])
-        pipe_group.remove(pipe_group.sprites()[0])
-
-        pipes = get_random_pipes(SCREEN_WIDHT * 2)
-
+    pipe_group = pygame.sprite.Group()
+    for i in range(2):
+        pipes = get_random_pipes(SCREEN_WIDHT * i + 800)
         pipe_group.add(pipes[0])
         pipe_group.add(pipes[1])
 
-    bird_group.update()
-    ground_group.update()
-    pipe_group.update()
+    clock = pygame.time.Clock()
+    score = 0
 
-    bird_group.draw(screen)
-    pipe_group.draw(screen)
-    ground_group.draw(screen)
+    # --- Màn hình bắt đầu ---
+    begin = True
+    while begin:
+        clock.tick(15)
 
-    pygame.display.update()
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE or event.key == K_UP:
+                    bird.bump()
+                    pygame.mixer.music.load(wing)
+                    pygame.mixer.music.play()
+                    begin = False
 
-    if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or
-            pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
-        pygame.mixer.music.load(hit)
-        pygame.mixer.music.play()
-        time.sleep(1)
-        break
+        screen.blit(BACKGROUND, (0, 0))
+        screen.blit(BEGIN_IMAGE, (120, 150))
 
+        if is_off_screen(ground_group.sprites()[0]):
+            ground_group.remove(ground_group.sprites()[0])
+            new_ground = Ground(GROUND_WIDHT - 20)
+            ground_group.add(new_ground)
+
+        bird.begin()
+        ground_group.update()
+
+        bird_group.draw(screen)
+        ground_group.draw(screen)
+
+        pygame.display.update()
+
+    # --- Vòng lặp chính ---
+    while True:
+        clock.tick(15)
+
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit()
+            if event.type == KEYDOWN:
+                if event.key == K_SPACE or event.key == K_UP:
+                    bird.bump()
+                    pygame.mixer.music.load(wing)
+                    pygame.mixer.music.play()
+
+        screen.blit(BACKGROUND, (0, 0))
+
+        # Cập nhật ground
+        if is_off_screen(ground_group.sprites()[0]):
+            ground_group.remove(ground_group.sprites()[0])
+            new_ground = Ground(GROUND_WIDHT - 20)
+            ground_group.add(new_ground)
+
+        # Cập nhật pipe
+        if is_off_screen(pipe_group.sprites()[0]):
+            pipe_group.remove(pipe_group.sprites()[0])
+            pipe_group.remove(pipe_group.sprites()[0])
+            pipes = get_random_pipes(SCREEN_WIDHT * 2)
+            pipe_group.add(pipes[0])
+            pipe_group.add(pipes[1])
+
+        # Đếm điểm: chim vượt qua ống dưới (không inverted)
+        bird_x = bird.rect[0] + bird.rect[2]  # cạnh phải của chim
+        for pipe in pipe_group.sprites():
+            if not pipe.passed and not hasattr(pipe, 'inverted_pipe'):
+                # Chỉ đếm ống dưới (rect[1] > 0)
+                if pipe.rect[1] > 0 and bird_x > pipe.rect[0] + pipe.rect[2]:
+                    pipe.passed = True
+                    score += 1
+                    pygame.mixer.music.load(point)
+                    pygame.mixer.music.play()
+
+        bird_group.update()
+        ground_group.update()
+        pipe_group.update()
+
+        bird_group.draw(screen)
+        pipe_group.draw(screen)
+        ground_group.draw(screen)
+
+        # Vẽ điểm
+        draw_score(screen, score)
+
+        pygame.display.update()
+
+        # Kiểm tra va chạm
+        if (pygame.sprite.groupcollide(bird_group, ground_group, False, False, pygame.sprite.collide_mask) or
+                pygame.sprite.groupcollide(bird_group, pipe_group, False, False, pygame.sprite.collide_mask)):
+            pygame.mixer.music.load(hit)
+            pygame.mixer.music.play()
+            time.sleep(1)
+            break
+
+    # --- Màn hình Game Over ---
+    show_gameover_screen(screen, score)
+
+
+# Vòng lặp ngoài: cho phép chơi lại nhiều lần
+while True:
+    run_game()
